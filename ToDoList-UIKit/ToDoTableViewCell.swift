1d092126aae13021571bdc54a63653023d55bc6d
Jihye Park <deviloper.park@gmail.com>
2024-05-24 11:18:02 +0900

    Add Subview & Set Constraint

--- ToDoList-UIKit/ToDoTableViewCell.swift
+++ ToDoList-UIKit/ToDoTableViewCell.swift
@@ -35,6 +35,32 @@ class ToDoTableViewCell: UITableViewCell {
         return label
     }()
     
+    /// 셀에 뷰 추가 및 제약조건 설정 초기화
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(checkboxButton)
+        addSubview(toDoTitle)
+        
+        checkboxButton.translatesAutoresizingMaskIntoConstraints = false
+        toDoTitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            checkboxButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            checkboxButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            checkboxButton.widthAnchor.constraint(equalToConstant: 30),
+            checkboxButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            toDoTitle.leadingAnchor.constraint(equalTo: checkboxButton.trailingAnchor, constant: 10),
+            toDoTitle.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            toDoTitle.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
