9cf9158a70e96a6dca5990f1c7a724c3b537f74e
Jihye Park <deviloper.park@gmail.com>
2024-05-24 10:18:55 +0900

    [feat] cancelButtonTapped

--- ToDoList-UIKit/AddToDoViewController.swift
+++ ToDoList-UIKit/AddToDoViewController.swift
@@ -15,15 +15,18 @@ class AddToDoViewController: UIViewController {
         self.view.backgroundColor = .white
         self.navigationItem.title = "Add ToDo"
         
+        /// 취소 버튼
         let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTapped))
         self.navigationItem.leftBarButtonItem = cancelButton
         
+        /// 확인 버튼
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
         self.navigationItem.rightBarButtonItem = doneButton
     }
     
     @objc func cancelButtonTapped() {
-        
+        /// 이전 화면으로 이동
+        self.navigationController?.popViewController(animated: true)
     }
     
     @objc func doneButtonTapped() {
